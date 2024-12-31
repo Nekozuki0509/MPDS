16255445da82394349d648845cd16d873d9479e2
Nekouzki <106313126+Nekozuki2009@users.noreply.github.com>
2025-01-01 05:01:29 +0900

    クラス分けして1.19.x対応

---
+++ src/main/java/mpds/mpds/events/Disconnect.java
@@ -0,0 +1,137 @@
+package mpds.mpds.events;
+
+import com.mojang.serialization.JsonOps;
+import com.mysql.cj.jdbc.exceptions.CommunicationsException;
+import mpds.mpds.mixin.HungerManagerAccessor;
+import mpds.mpds.mixin.PlayerManagerInvoker;
+import net.minecraft.inventory.EnderChestInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.collection.DefaultedList;
+
+import java.sql.ResultSet;
+
+import static mpds.mpds.MPDS.*;
+
+public class Disconnect {
+    public static void ondisconnect(ServerPlayNetworkHandler serverPlayNetworkHandler, MinecraftServer minecraftServer) {
+        new Thread(() -> {
+            ServerPlayerEntity player = serverPlayNetworkHandler.getPlayer();
+            LOGGER.info("saving {}'s data...", player.getName().getString());
+
+            while (true) {
+                try {
+                    checkskip.setString(1, player.getName().getString());
+                    ResultSet checkskiprs = checkskip.executeQuery();
+
+                    if (checkskiprs.next() && "true".equals(checkskiprs.getString("skip"))) {
+                        if (ASM)
+                            minecraftServer.getPlayerManager().broadcast(Text.translatable("skip saving because " + player.getName().getString() + "'s data includes skip list").formatted(Formatting.YELLOW), false);
+                        LOGGER.warn("skip saving because {}'s data includes skip list", player.getName().getString());
+
+                        player.getWorld().playSound(null, player.getBlockPos(), SoundEvents.BLOCK_GLASS_BREAK, SoundCategory.PLAYERS, 1f, 1f);
+                        bea.setString(1, player.getUuidAsString());
+                        bea.executeUpdate();
+                        befalse.setString(1, player.getUuid().toString());
+                        befalse.executeUpdate();
+
+                        return;
+                    }
+
+                    if (broken.stream().anyMatch(bplayer -> bplayer.equals(player.getUuid()))) {
+                        LOGGER.warn("skip saving because {}'s data was broken", player.getName().getString());
+                        broken.remove(player.getUuid());
+
+                        player.getInventory().clear();
+                        player.getEnderChestInventory().clear();
+                        player.clearStatusEffects();
+                        ((PlayerManagerInvoker) minecraftServer.getPlayerManager()).invokesavePlayerData(player);
+
+                        return;
+                    }
+
+                    ondisconnectstatement.setString(1, player.getName().getString());
+                    ondisconnectstatement.setString(2, player.getUuidAsString());
+
+                    if (SA) ondisconnectstatement.setInt(3, player.getAir());
+
+                    if (SH) ondisconnectstatement.setFloat(4, player.getHealth());
+
+                    if (SF) {
+                        ondisconnectstatement.setFloat(6, player.getHungerManager().getExhaustion());
+                        ondisconnectstatement.setInt(7, player.getHungerManager().getFoodLevel());
+                        ondisconnectstatement.setFloat(8, player.getHungerManager().getSaturationLevel());
+                        ondisconnectstatement.setInt(9, ((HungerManagerAccessor) player.getHungerManager()).getFoodTickTimer());
+                    }
+
+                    if (SL) {
+                        ondisconnectstatement.setInt(14, player.experienceLevel);
+                        ondisconnectstatement.setFloat(15, player.experienceProgress);
+                    }
+
+                    if (SEn) {
+                        EnderChestInventory end = player.getEnderChestInventory();
+                        StringBuilder endresults = new StringBuilder();
+                        for (int i = 0; i < end.size(); i++) {
+                            if (end.getStack(i).isEmpty()) continue;
+                            endresults.append(ItemStack.CODEC.encodeStart(JsonOps.INSTANCE, end.getStack(i)).resultOrPartial(LOGGER::error).orElseThrow()).append("~").append(i).append("&");
+                        }
+                        ondisconnectstatement.setString(5, endresults.toString());
+
+                        player.getEnderChestInventory().clear();
+                    }
+
+                    if (SI) {
+                        ondisconnectstatement.setString(11, player.getInventory().offHand.get(0).isEmpty() ? "" : ItemStack.CODEC.encodeStart(JsonOps.INSTANCE, player.getInventory().offHand.get(0)).resultOrPartial(LOGGER::error).orElseThrow().toString());
+                        ondisconnectstatement.setInt(13, player.getInventory().selectedSlot);
+
+                        DefaultedList<ItemStack> main = player.getInventory().main;
+                        StringBuilder mainresults = new StringBuilder();
+                        for (int i = 0; i < main.size(); i++) {
+                            if (main.get(i).isEmpty()) continue;
+                            mainresults.append(ItemStack.CODEC.encodeStart(JsonOps.INSTANCE, main.get(i)).resultOrPartial(LOGGER::error).orElseThrow()).append("~").append(i).append("&");
+                        }
+                        ondisconnectstatement.setString(10, mainresults.toString());
+
+                        DefaultedList<ItemStack> armor = player.getInventory().armor;
+                        StringBuilder armorresults = new StringBuilder();
+                        for (int i = 0; i < armor.size(); i++) {
+                            if (armor.get(i).isEmpty()) continue;
+                            armorresults.append(ItemStack.CODEC.encodeStart(JsonOps.INSTANCE, armor.get(i)).resultOrPartial(LOGGER::error).orElseThrow()).append("~").append(i).append("&");
+                        }
+                        ondisconnectstatement.setString(12, armorresults.toString());
+
+                        player.getInventory().clear();
+                    }
+
+                    if (SEf) {
+                        StringBuilder effectresults = new StringBuilder();
+                        player.getStatusEffects().forEach(effect -> effectresults.append(NbtCompound.CODEC.encodeStart(JsonOps.INSTANCE, effect.writeNbt(new NbtCompound())).resultOrPartial(LOGGER::error).orElseThrow()).append("&"));
+                        ondisconnectstatement.setString(16, effectresults.toString());
+                        ondisconnectstatement.executeUpdate();
+
+                        player.clearStatusEffects();
+                    }
+
+                    ((PlayerManagerInvoker) minecraftServer.getPlayerManager()).invokesavePlayerData(player);
+                    LOGGER.info("success to save {}'s data", player.getName().getString());
+
+                    return;
+                } catch (CommunicationsException ignored) {
+                } catch (Exception e) {
+                    LOGGER.error("FAIL TO SAVE {}'s DATA:", player.getName().getString());
+                    e.printStackTrace();
+
+                    return;
+                }
+            }
+        }).start();
+    }
+}
---
+++ src/main/java/mpds/mpds/events/Join.java
@@ -0,0 +1,177 @@
+package mpds.mpds.events;
+
+import com.google.gson.JsonParser;
+import com.mojang.serialization.JsonOps;
+import com.mysql.cj.jdbc.exceptions.CommunicationsException;
+import mpds.mpds.mixin.HungerManagerAccessor;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
+import net.minecraft.registry.Registries;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.random.Random;
+
+import java.sql.ResultSet;
+import java.util.List;
+
+import static mpds.mpds.MPDS.*;
+
+public class Join {
+    public static void onjoin(ServerPlayNetworkHandler serverPlayNetworkHandler, PacketSender packetSender, MinecraftServer minecraftServer) {
+        new Thread(() -> {
+            ServerPlayerEntity player = serverPlayNetworkHandler.getPlayer();
+            broken.add(player.getUuid());
+
+            if (AJM)
+                player.sendMessage(Text.translatable("loading " + player.getName().getString() + "'s data...").formatted(Formatting.YELLOW));
+            LOGGER.info("loading {}'s data...", player.getName().getString());
+
+            while (true) {
+                try {
+                    checkskip.setString(1, player.getName().getString());
+                    ResultSet checkskiprs = checkskip.executeQuery();
+
+                    if (checkskiprs.next() && "true".equals(checkskiprs.getString("skip"))) {
+                        if (ASM) {
+                            minecraftServer.getPlayerManager().broadcast(Text.translatable("skip loading because " + player.getName().getString() + "'s data includes skip list").formatted(Formatting.YELLOW), false);
+                            player.sendMessage(Text.translatable("skip loading because " + player.getName().getString() + "'s data includes skip list").formatted(Formatting.YELLOW));
+                        }
+                        LOGGER.warn("skip loading because {}'s data includes skip list", player.getName().getString());
+
+                        broken.remove(player.getUuid());
+
+                        player.networkHandler.sendPacket(new PlaySoundS2CPacket(Registries.SOUND_EVENT.getEntry(SoundEvents.BLOCK_GLASS_BREAK), SoundCategory.PLAYERS, player.getX(), player.getY(), player.getZ(), 1f, 1f, Random.createThreadSafe().nextLong()));
+                        player.getWorld().playSound(null, player.getBlockPos(), SoundEvents.BLOCK_GLASS_BREAK, SoundCategory.PLAYERS, 1f, 1f);
+
+                        return;
+                    }
+                    onjoinstatement.setString(1, player.getUuid().toString());
+
+                    ResultSet resultSet;
+                    if ((resultSet = onjoinstatement.executeQuery()).next()) {
+                        for (int i = 0; "false".equals(resultSet.getString("sync")); i++) {
+                            if (i == 10) {
+                                if (ServerName.equals(resultSet.getString("server")) || "*".equals(resultSet.getString("server"))) {
+                                    if (AJM)
+                                        player.sendMessage(Text.translatable("saved " + player.getName().getString() + "'s correct data").formatted(Formatting.AQUA));
+                                    LOGGER.info("saved {}'s correct data", player.getName().getString());
+
+                                    broken.remove(player.getUuid());
+                                    player.networkHandler.sendPacket(new PlaySoundS2CPacket(Registries.SOUND_EVENT.getEntry(SoundEvents.ENTITY_PLAYER_LEVELUP), SoundCategory.PLAYERS, player.getX(), player.getY(), player.getZ(), 1f, 1f, Random.createThreadSafe().nextLong()));
+
+                                    return;
+                                }
+                                if (AEM)
+                                    player.sendMessage(Text.translatable("IT LOOKS " + player.getName().getString() + "'s DATA WAS BROKEN!\nPLEASE CONNECT TO " + resultSet.getString("server") + "!").formatted(Formatting.RED));
+                                LOGGER.error("IT LOOKS {}'s DATA WAS BROKEN!\nPLEASE CONNECT TO {}!", player.getName().getString(), resultSet.getString("server"));
+                                player.networkHandler.sendPacket(new PlaySoundS2CPacket(Registries.SOUND_EVENT.getEntry(SoundEvents.BLOCK_ANVIL_DESTROY), SoundCategory.PLAYERS, player.getX(), player.getY(), player.getZ(), 1f, 1f, Random.createThreadSafe().nextLong()));
+
+                                return;
+                            }
+                            Thread.sleep(1000);
+                            resultSet = onjoinstatement.executeQuery();
+                            resultSet.next();
+                        }
+                        befalse.setString(1, player.getUuid().toString());
+                        befalse.executeUpdate();
+
+                        if (SA) player.setAir(resultSet.getInt("Air"));
+
+                        if (SH) player.setHealth(resultSet.getFloat("Health"));
+
+                        if (SF) {
+                            player.getHungerManager().setExhaustion(resultSet.getFloat("exhaustion"));
+                            player.getHungerManager().setFoodLevel(resultSet.getInt("foodLevel"));
+                            player.getHungerManager().setSaturationLevel(resultSet.getFloat("saturationLevel"));
+                            ((HungerManagerAccessor) player.getHungerManager()).setFoodTickTimer(resultSet.getInt("foodTickTimer"));
+                        }
+
+                        if (SL) {
+                            player.experienceLevel = resultSet.getInt("experienceLevel");
+                            player.experienceProgress = resultSet.getInt("experienceProgress");
+                        }
+
+                        if (SEn && !"".equals(resultSet.getString("enderChestInventory")))
+                            List.of(resultSet.getString("enderChestInventory").split("&")).forEach(compound -> {
+                                String[] compounds = compound.split("~");
+                                player.getEnderChestInventory().setStack(Integer.parseInt(compounds[1]), ItemStack.CODEC.parse(JsonOps.INSTANCE, JsonParser.parseString(compounds[0])).resultOrPartial(LOGGER::error).orElseThrow());
+                            });
+
+                        if (SI) {
+                            if (!"".equals(resultSet.getString("off")))
+                                player.getInventory().offHand.set(0, ItemStack.CODEC.parse(JsonOps.INSTANCE, JsonParser.parseString(resultSet.getString("off"))).resultOrPartial(LOGGER::error).orElseThrow());
+                            player.getInventory().selectedSlot = resultSet.getInt("selectedSlot");
+                            if (!"".equals(resultSet.getString("main"))) {
+                                List.of(resultSet.getString("main").split("&")).forEach(compound -> {
+                                    String[] compounds = compound.split("~");
+                                    player.getInventory().main.set(Integer.parseInt(compounds[1]), ItemStack.CODEC.parse(JsonOps.INSTANCE, JsonParser.parseString(compounds[0])).resultOrPartial(LOGGER::error).orElseThrow());
+                                });
+                            }
+                            if (!"".equals(resultSet.getString("armor"))) {
+                                List.of(resultSet.getString("armor").split("&")).forEach(compound -> {
+                                    String[] compounds = compound.split("~");
+                                    player.getInventory().armor.set(Integer.parseInt(compounds[1]), ItemStack.CODEC.parse(JsonOps.INSTANCE, JsonParser.parseString(compounds[0])).resultOrPartial(LOGGER::error).orElseThrow());
+                                });
+                            }
+                        }
+
+                        if (SEf && !"".equals(resultSet.getString("effects")))
+                            List.of(resultSet.getString("effects").split("&")).forEach(compound -> player.addStatusEffect(StatusEffectInstance.fromNbt(NbtCompound.CODEC.parse(JsonOps.INSTANCE, JsonParser.parseString(compound)).resultOrPartial(LOGGER::error).orElseThrow())));
+
+                        if (AJM)
+                            player.sendMessage(Text.translatable("success to load " + player.getName().getString() + "'s data!").formatted(Formatting.AQUA));
+                        LOGGER.info("success to load {}'s data!", player.getName().getString());
+
+                        player.networkHandler.sendPacket(new PlaySoundS2CPacket(Registries.SOUND_EVENT.getEntry(SoundEvents.ENTITY_PLAYER_LEVELUP), SoundCategory.PLAYERS, player.getX(), player.getY(), player.getZ(), 1f, 1f, Random.createThreadSafe().nextLong()));
+
+                        setserver.setString(1, ServerName);
+                        setserver.setString(2, player.getUuid().toString());
+                        setserver.executeUpdate();
+
+                        broken.remove(player.getUuid());
+                    } else {
+                        Thread.sleep(1000);
+
+                        for (int i = 1; !onjoinstatement.executeQuery().next(); i++) {
+                            if (i == 10) {
+                                if (AEM)
+                                    player.sendMessage(Text.translatable("COULD NOT FIND " + player.getName().getString() + "'s DATA!\nMADE NEW ONE!").formatted(Formatting.RED));
+                                LOGGER.warn("COULD NOT FIND {}'s DATA!\nMADE NEW ONE!", player.getName().getString());
+
+                                player.networkHandler.sendPacket(new PlaySoundS2CPacket(Registries.SOUND_EVENT.getEntry(SoundEvents.BLOCK_GLASS_BREAK), SoundCategory.PLAYERS, player.getX(), player.getY(), player.getZ(), 1f, 1f, Random.createThreadSafe().nextLong()));
+                                broken.remove(player.getUuid());
+
+                                return;
+                            }
+                            Thread.sleep(1000);
+                        }
+                    }
+
+                    return;
+                } catch (CommunicationsException ignored) {
+                } catch (Exception e) {
+                    player.getInventory().clear();
+                    player.getEnderChestInventory().clear();
+                    player.clearStatusEffects();
+
+                    if (AEM)
+                        player.sendMessage(Text.translatable("THERE WERE SOME ERRORS WHEN LOAD " + player.getName().getString() + "'s DATA! : \n" + e.getMessage()).formatted(Formatting.RED));
+                    LOGGER.error("THERE WERE SOME ERRORS WHEN LOAD {}'s DATA!:", player.getName().getString());
+
+                    player.networkHandler.sendPacket(new PlaySoundS2CPacket(Registries.SOUND_EVENT.getEntry(SoundEvents.BLOCK_ANVIL_DESTROY), SoundCategory.PLAYERS, player.getX(), player.getY(), player.getZ(), 1f, 1f, Random.createThreadSafe().nextLong()));
+                    e.printStackTrace();
+
+                    return;
+                }
+            }
+        }).start();
+    }
+}
